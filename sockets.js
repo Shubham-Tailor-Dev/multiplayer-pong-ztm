dcefda54ce041a3fc69c1b7e04c8132600cfacb0
Tailor Shubham <tailor.shubham.dev@gmail.com>
2024-11-01 15:59:25 +0530

    added rooms

--- sockets.js
+++ sockets.js
@@ -3,27 +3,29 @@ let readyPlayerCount = 0;
 function listen(io) {
   const pongNamespace = io.of("/pong"); // nameSpace specific to "/pong"
   pongNamespace.on("connection", (socket) => {
+    let room;
     console.log("a user connected", socket.id);
-
     socket.on("ready", () => {
-      console.log("Player Ready", socket.id);
-
+      room = "room" + Math.floor(readyPlayerCount / 2);
+      console.log('Player ready', socket.id, room);
+      socket.join(room);
       readyPlayerCount++;
 
       if (readyPlayerCount % 2 === 0) {
-        pongNamespace.emit("startGame", socket.id);
+        pongNamespace.in(room).emit("startGame", socket.id);
       }
 
       socket.on("paddleMove", (paddleData) => {
-        socket.broadcast.emit("paddleMove", paddleData);
+        socket.to(room).emit("paddleMove", paddleData);
       });
 
       socket.on("ballMove", (ballData) => {
-        socket.broadcast.emit("ballMove", ballData);
+        socket.to(room).emit("ballMove", ballData);
       });
 
       socket.on("disconnect", (reason) => {
         console.log(`Client ${socket.id} disconnected: ${reason}`);
+        socket.leave(room);
       });
     });
   });
